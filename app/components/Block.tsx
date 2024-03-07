d6729dbd2e5737f8fffaade9e40e037ce9be203b
Dave Townsend <dtownsend@oxymoronical.com>
2024-03-07 22:38:31 +0000

    Allow for measuring and position blocks

--- app/components/Block.tsx
+++ app/components/Block.tsx
@@ -1,26 +1,38 @@
+import { useMemo } from "react";
+
 import { BlockConfig } from "../modules/config";
-import { BlockLayout } from "~/modules/layout";
+import { BlockLayout, Metrics, getPosition } from "~/modules/layout";
 
 export default function Block({
   layout,
   config,
+  metrics,
 }: {
   layout: BlockLayout;
   config: BlockConfig;
+  metrics?: Metrics | null;
 }) {
-  let height = config.height ?? 1;
-  let width = config.width ?? 1;
+  let styles = useMemo((): React.CSSProperties => {
+    if (metrics) {
+      return getPosition(
+        metrics,
+        layout.x,
+        layout.y,
+        config.width,
+        config.height,
+      );
+    }
+
+    return {
+      gridColumnStart: layout.x + 1,
+      gridColumnEnd: `span ${config.width}`,
+      gridRowStart: layout.y + 1,
+      gridRowEnd: `span ${config.height}`,
+    };
+  }, [layout, config, metrics]);
 
   return (
-    <div
-      className="block"
-      style={{
-        gridColumnStart: layout.x + 1,
-        gridColumnEnd: `span ${width}`,
-        gridRowStart: layout.y + 1,
-        gridRowEnd: `span ${height}`,
-      }}
-    >
+    <div className="block" style={styles}>
       <div className="title">
         <h1>{config.title}</h1>
         <div className="buttons" />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,28 +1,60 @@
 import type { MetaFunction } from "@remix-run/node";
 
+import clsx from "clsx";
+import { useMemo, useState } from "react";
+
 import { useAppContext } from "~/components/AppContext";
 import Block from "~/components/Block";
+import { Metrics } from "~/modules/layout";
 
 export const meta: MetaFunction = () => [{ title: "Am I Done Yet?" }];
 
 export default function Index() {
   let appContext = useAppContext();
 
+  let metricCells = useMemo(() => {
+    let cells: string[] = [];
+
+    cells.push("col1");
+    if (appContext.columns > 1) {
+      cells.push("col2");
+    }
+    cells.push("row2");
+
+    return cells;
+  }, [appContext.columns]);
+
+  let [metrics] = useState<Metrics | null>(null);
+
   return (
-    <div
-      id="gridroot"
-      style={{
-        gridTemplateColumns: `repeat(${appContext.columns}, 1fr)`,
-        gridAutoRows: `${appContext.rowHeight}px`,
-      }}
-    >
-      {appContext.layouts.map((blockLayout) => (
-        <Block
-          key={blockLayout.id}
-          layout={blockLayout}
-          config={appContext.blockConfigs[blockLayout.id]}
-        />
-      ))}
-    </div>
+    <main>
+      <div
+        className="metrics blockgrid"
+        style={{
+          gridTemplateColumns: `repeat(${appContext.columns}, 1fr)`,
+          gridAutoRows: `${appContext.rowHeight}px`,
+        }}
+      >
+        {metricCells.map((c) => (
+          <div key={c} className={c} />
+        ))}
+      </div>
+      <div
+        className={clsx("blocks", metrics ? "fixed" : "blockgrid")}
+        style={{
+          gridTemplateColumns: `repeat(${appContext.columns}, 1fr)`,
+          gridAutoRows: `${appContext.rowHeight}px`,
+        }}
+      >
+        {appContext.layouts.map((blockLayout) => (
+          <Block
+            key={blockLayout.id}
+            metrics={metrics}
+            layout={blockLayout}
+            config={appContext.blockConfigs[blockLayout.id]}
+          />
+        ))}
+      </div>
+    </main>
   );
 }
